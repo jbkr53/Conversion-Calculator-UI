de1c1dddadaf46f9ff102faecd3a62835227027f
jbkr53 <you@yourdomain.comjbkr53@mail.umkc.edu>
2019-10-31 15:23:04 -0500

    IB outlets and actions added

--- ConversionCalc/ConversionCalc/ConversionViewController.swift
+++ ConversionCalc/ConversionCalc/ConversionViewController.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 James Kirkland. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet weak var outputdisplay: UITextField!    
+
+    @IBOutlet weak var inputdisplay: UITextField!
+    
+    @IBAction func convert(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var conversions: [Convert] = [Convert(label:"Celsius to Farenheight", input_unit: "°C", output_unit: "°F"), Convert(label:"Farenheight to Celsius", input_unit: "°F", output_unit: "°C"), Convert(label:"miles to kilometers", input_unit: "mi", output_unit: "km"), Convert(label:"kilometers to miles", input_unit: "km", output_unit: "mi")]
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ConversionCalc/ConversionCalc/convert.swift
@@ -0,0 +1,15 @@
+//
+//  convert.swift
+//  ConversionCalc
+//
+//  Created by James Kirkland on 10/31/19.
+//  Copyright © 2019 James Kirkland. All rights reserved.
+//
+
+import Foundation
+
+struct Convert {
+       var label:String
+       var input_unit:String
+       var output_unit:String
+   }
